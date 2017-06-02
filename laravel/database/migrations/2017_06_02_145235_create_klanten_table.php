47c3670caa2a10244afec803d04a461c63aa4373
Luquatic <jesseyfransen@live.nl>
2017-06-02 16:59:05 +0200

    added migrations

--- laravel/database/migrations/2017_06_02_145235_create_klanten_table.php
+++ laravel/database/migrations/2017_06_02_145235_create_klanten_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateKlantenTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('klanten', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('naam');
+            $table->string('wachtwoord');
+            $table->unique('user_id');
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('klanten');
     }
 }
