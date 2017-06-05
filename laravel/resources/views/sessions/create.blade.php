b6585b67e5e833c7f0301f9083827b4e34132516
Luquatic <jesseyfransen@live.nl>
2017-06-05 12:55:05 +0200

    added register page

--- laravel/resources/views/sessions/create.blade.php
+++ laravel/resources/views/sessions/create.blade.php
@@ -8,12 +8,15 @@
             <input type="text" name="id" placeholder="Klantnummer" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Klantnummer'"/>
         </div>
         <div class="formInput">
-            <input type="text" name="naam" placeholder="Naam en achternaam" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Klantnummer'"/>
+            <input type="text" name="voornaam" placeholder="Voornaam" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Voornaam'"/>
+        </div>
+        <div class="formInput">
+            <input type="text" name="achternaam" placeholder="Achternaam" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Achternaam'"/>
         </div>
         <div class="formInput">
             <input type="password" name="password" placeholder="Code" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Code'"/>
         </div>
         <?php if(isset($fmsg)) { ?> <div class="alert alertFailure" role="alert"> <?php echo $fmsg; ?> </div> <?php } ?>
-        <button class="btn btnLogin" type="submit">LOGIN</button>
+        <button class="btn btnRegister" type="submit">REGISTER</button>
     </form>
 @endsection
