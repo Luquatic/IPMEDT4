8a8046e3f959894150837320837ee90ed68aee5f
Luquatic <jesseyfransen@live.nl>
2017-06-06 04:57:47 +0200

    small changes

--- laravel/app/Http/Middleware/RedirectIfAuthenticated.php
+++ laravel/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/homeController');
+            return redirect('/home');
         }
 
         return $next($request);
