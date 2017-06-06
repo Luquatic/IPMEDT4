8ffe7740f343c48efebf07c8d6bc53039d558c8a
Luquatic <jesseyfransen@live.nl>
2017-06-06 03:26:59 +0200

    small changes

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -9,14 +9,8 @@
 
         {{ csrf_field() }}
 
-        <div class="formInput{{ $errors->has('id') ? ' has-error' : '' }}">
+        <div class="formInput">
             <input type="text" name="id" placeholder="Klantnummer" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Klantnummer'"/>
-
-            @if ($errors->has('id'))
-                <span class="help-block">
-                                        <strong>{{ $errors->first('id') }}</strong>
-                                    </span>
-            @endif
         </div>
 
         <div class="formInput">
@@ -27,14 +21,8 @@
             <input type="text" name="achternaam" placeholder="Achternaam" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Achternaam'"/>
         </div>
 
-        <div class="formInput{{ $errors->has('wachtwoord') ? ' has-error' : '' }}">
+        <div class="formInput">
             <input type="password" name="wachtwoord" placeholder="Code" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Code'"/>
-
-            @if ($errors->has('password'))
-                <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-            @endif
         </div>
 
         <?php if(isset($fmsg)) { ?> <div class="alert alertFailure" role="alert"> <?php echo $fmsg; ?> </div> <?php } ?>
