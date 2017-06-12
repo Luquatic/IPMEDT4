8eddb76f0aa71eb301acf11a7275b884ea1fd13e
Luquatic <jesseyfransen@live.nl>
2017-06-12 19:20:44 +0200

    small changes

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -14,6 +14,10 @@
             <input type="password" name="password" placeholder="Code" required onfocus="this.placeholder =''" onblur="this.placeholder = 'Code'"/>
         </div>
 
+        @if($errors->any())
+            <h4>{{$errors->first()}}</h4>
+        @endif
+
         <button class="btn btnLogin" type="submit">INLOGGEN</button>
 
     </form>
