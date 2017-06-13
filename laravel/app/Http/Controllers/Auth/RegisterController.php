0bdccffa91b375ed600a23db810d4cf7726c6e59
Luquatic <jesseyfransen@live.nl>
2017-06-13 14:59:46 +0200

    trying to fix registration with bcrypt

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
             'klant_id' => $data['klant_id'],
             'voornaam' => $data['voornaam'],
             'achternaam' => $data['achternaam'],
-            'password' => $data['password'],
+            'password' => $data[bcrypt('password')],
         ]);
     }
 }
