c73fade0045d9668e37d80fd31c7084291fa6a85
Luquatic <jesseyfransen@live.nl>
2017-06-21 13:06:25 +0200

    added stuff for medt grades

---
+++ laravel/database/migrations/2017_06_21_105033_create_vakken_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVakkenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vakken', function (Blueprint $table) {
+            $table->integer('jaar');
+            $table->integer('periode');
+            $table->string('naam')->primary();
+            $table->integer('ec');
+            $table->boolean('gehaald');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -17,5 +17,159 @@ class DatabaseSeeder extends Seeder
             'achternaam' => 'Fransen',
             'password' => bcrypt('admin'),
         ]);
+
+        // Periode 1
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 1,
+            'naam' => 'IARCH',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 1,
+            'naam' => 'IIBPM',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 1,
+            'naam' => 'IHBO',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 1,
+            'naam' => 'IOPR1',
+            'ec' => 4,
+            'gehaald' => false,
+        ]);
+
+        // Periode 2
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 2,
+            'naam' => 'IWDR',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 2,
+            'naam' => 'IRDB',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 2,
+            'naam' => 'IIBUI',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 2,
+            'naam' => 'INET',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 2,
+            'naam' => 'IPRODAM',
+            'ec' => 2,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 2,
+            'naam' => 'IPOMEDT',
+            'ec' => 2,
+            'gehaald' => false,
+        ]);
+
+        // Periode 3
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 3,
+            'naam' => 'IMUML',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 3,
+            'naam' => 'IOPR2',
+            'ec' => 4,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 3,
+            'naam' => 'IFIT',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 3,
+            'naam' => 'IPOFIT',
+            'ec' => 2,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 3,
+            'naam' => 'IPOSE',
+            'ec' => 2,
+            'gehaald' => false,
+        ]);
+
+        // Periode 4
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 4,
+            'naam' => 'IPROV',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 4,
+            'naam' => 'ICOMMP',
+            'ec' => 3,
+            'gehaald' => false,
+        ]);
+
+        DB::table('vakken')->insert([
+            'jaar' => 1,
+            'periode' => 4,
+            'naam' => 'ISLP',
+            'ec' => 1,
+            'gehaald' => false,
+        ]);
+
+
     }
 }
