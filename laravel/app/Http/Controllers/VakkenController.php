6be005ddd144190488af6d12b97e04d0a886b04f
Luquatic <jesseyfransen@live.nl>
2017-06-22 15:26:09 +0200

    added jaar to medtgrades

--- laravel/app/Http/Controllers/VakkenController.php
+++ laravel/app/Http/Controllers/VakkenController.php
@@ -14,4 +14,8 @@ class VakkenController extends Controller
     public function jaar($jaar) {
         return Vak::where('jaar', $jaar)->get();
     }
+
+    public function periode($jaar, $periode) {
+        return Vak::where('jaar', $jaar)->where('periode', $periode)->get();
+    }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/medtgrades','VakkenController@create');
 Route::get('/medtgrades/jaar/{jaar}', 'VakkenController@jaar');
+Route::get('/medtgrades/jaar/{jaar}/periode/{periode}', 'VakkenController@periode');
